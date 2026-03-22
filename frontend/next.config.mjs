7db5813f40f23c9c5bc1d22526f31b2826a566fd
andrebontempo <andre.lnx@gmail.com>
2026-03-22 12:49:00 -0300

    Remove `trailingSlash` and commented `output` properties from Next.js configuration.

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,7 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  // output: 'export', // Removido para suportar o SSR do "npm run start" nativo no Docker
-  trailingSlash: true,
-};
+const nextConfig = {};
 
 export default nextConfig;
